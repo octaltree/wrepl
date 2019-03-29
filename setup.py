c3135d59a41313007e916edc61655d86e566567b
octaltree <octaltree@gmail.com>
2019-03-29 20:11:58 +0900

    tmp

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
         name='wrepl',
-        version="0.0.11",
+        version="0.0.12",
         description="Watch-Read-Eval-Print Loop",
         url='https://github.com/octaltree/wrepl',
         author='octaltree',
