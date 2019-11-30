69a909dae722a06f8f16a2d938255b403af93e40
octaltree <octaltree@gmail.com>
2019-11-30 22:40:19 +0900

    private

--- dev/script.py
+++ dev/script.py
@@ -3,24 +3,6 @@ from itertools import zip_longest
 from cell import Cell
 from getter import getter
 
-def equal(na, nb):
-    if type(na) is not type(nb): return False
-    if isinstance(na, list):
-        if len(na) != len(nb): return False
-        for i in range(len(na)):
-            if not equal(na[i], nb[i]):
-                return False
-        return True
-    if not isinstance(na, ast.AST): return na == nb
-    # only AST
-    if set(na._fields) != set(nb._fields): return False
-    for fn in na._fields:
-        va = getattr(na, fn)
-        vb = getattr(nb, fn)
-        if equal(va, vb): continue
-        return False
-    return True
-
 class Script:
     def __init__(self, fileName, raw):
         self.fileName = fileName
@@ -41,6 +23,24 @@ class Script:
                 Cell(self.fileName, extract(self.raw, c, n), c)
                 for (c, n) in zip_longest(self.ast.body, self.ast.body[1:])]
 
+def _equal(na, nb):
+    if type(na) is not type(nb): return False
+    if isinstance(na, list):
+        if len(na) != len(nb): return False
+        for i in range(len(na)):
+            if not _equal(na[i], nb[i]):
+                return False
+        return True
+    if not isinstance(na, ast.AST): return na == nb
+    # only AST
+    if set(na._fields) != set(nb._fields): return False
+    for fn in na._fields:
+        va = getattr(na, fn)
+        vb = getattr(nb, fn)
+        if _equal(va, vb): continue
+        return False
+    return True
+
 class Differ:
     def __init__(self, before, after):
         self.before = before
@@ -51,7 +51,7 @@ class Differ:
         stmts = ([c.stmt for c in self.before.cells], [c.stmt for c in self.after.cells])
         num = 0
         for (p, n) in zip_longest(*stmts):
-            if not equal(p, n): break
+            if not _equal(p, n): break
             num += 1
         return num
 
@@ -61,7 +61,7 @@ class Differ:
         return self.after.cells[self.numSameStmts:]
 
 if __name__ == '__main__':
-    assert equal(ast.parse('3\n2'), ast.parse('3;2'))
-    assert not equal(ast.parse('b = 3'), ast.parse('a = 3'))
-    assert not equal(ast.parse('a = [1,3]\na'), ast.parse('a= [2,3]; a'))
+    assert _equal(ast.parse('3\n2'), ast.parse('3;2'))
+    assert not _equal(ast.parse('b = 3'), ast.parse('a = 3'))
+    assert not _equal(ast.parse('a = [1,3]\na'), ast.parse('a= [2,3]; a'))
     print(Differ(Script('foo', 'a = [1,3]\na'), Script('foo', 'a=[2,3]; a')).added())
